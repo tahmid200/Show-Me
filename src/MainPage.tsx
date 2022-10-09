d1eaabdf27ec8b7c2ce40069b5422a6d75fd213e
Tahmid Chowdhury <tahmid808@gmail.com>
2022-10-09 11:57:58 -0600

    Completed search and displays contents from data.

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import './MainPage.css';
+import JASONDATA from './Data.json';
 
 const MainPage = () => {
+    
+    const [searchItem, setSearchItem] = useState('');
+    
     return(
         <div>
-        <h1>Jackets Most popular today</h1>
-        <input type='text' placeholder='Search...' />
+            <div>
+                <h1>Jackets Most popular today</h1>
+            </div>
+            <div className="searchBar">
+                <input type='text' 
+                    placeholder='Search...'
+                    onChange={(event) => {
+                        setSearchItem(event.target.value);
+                    }} 
+                />
+                <div>
+                <button>All</button>
+                <button>Walking</button>
+                <button>Running</button>
+            </div>
+            {/* displaying the items in data with matching search fields */}
+            <br />
+                {JASONDATA.filter((val) => {
+                    if (searchItem == ''){
+                        return val;
+                    } else if (val.name.toLowerCase().includes(searchItem.toLowerCase())) {
+                        return val;
+                    }
+                }).map((val,key) => {
+                return (
+                    <div className="createCards" key={key}>
+                        <img src={(val.image)}/>
+                        <p>{val.price}</p>
+                        <p>{val.name}</p> 
+                    </div>
+                );
+                })}
+            </div>
+            
         </div>
     );
 }
